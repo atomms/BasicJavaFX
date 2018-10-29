58b18d8cafeb39ecdcf98acad9bea4953a339df8
Ernesto Ramiro <atomms@gmail.com>
2018-10-29 17:12:04 +0100

    rid of initialize

--- BasicJavaFX/src/application/Main.java
+++ BasicJavaFX/src/application/Main.java
@@ -3,11 +3,8 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -31,7 +28,7 @@ public class Main extends Application {
                 "forest.fxml"
             )
         );
-//    	loader.setController(new SetBlur());
+
         mypane = (AnchorPane) loader.load();
 
         /**
@@ -41,29 +38,6 @@ public class Main extends Application {
         stage.setScene(new Scene(mypane));
         stage.show();
     }
-   
-
-    	
-    /**
-     * la clase controladora que añade un efecto a la imagen
-     */
-    private class SetBlur {
-    	
-    	@FXML
-        private ImageView imageView;       
-
-/**
- * inicializa al igual que el constructor pero permite acceder al componente imageView ya creado en fxml
- */
-    	@FXML
-        public void initialize() {           
-                 imageView.setEffect(new GaussianBlur(10));
-        }
-    	
-    }
-   
-
-    
 
 
     /**
