6894833affa30325f36ef3f9be9b1de48f6f344c
Ernesto Ramiro <atomms@gmail.com>
2019-10-15 16:56:35 +0200

    clean up

--- BasicJavaFX/src/application/BlurController.java
+++ BasicJavaFX/src/application/BlurController.java
@@ -8,37 +8,12 @@ public class BlurController {
 	
 	@FXML
     private ImageView imageView;   
-//	private Ellipse ellipse;
-	
-//	@FXML
-//	private Pane mypane;   
 
-	
-//	@FXML
-//    public void setclip() {           
-//        ellipse = new Ellipse();
-////        circle = new Circle((imageView.getFitWidth())*0.2);
-//
-//        ellipse.centerXProperty().setValue(120);
-//        ellipse.centerYProperty().setValue(40);
-//        ellipse.radiusXProperty().setValue(90);
-//        ellipse.radiusYProperty().setValue(90);
-//        
-////        importing and modifying sizes
-////    	ellipse.radiusXProperty().setValue(imageView.getFitWidth()*0.1);
-////    	ellipse.radiusYProperty().setValue(imageView.getFitHeight()*0.17);
-//        
-//        imageView.setClip(ellipse);
-//
-//////apply a shadow effect.
-////        imageView.setEffect(new DropShadow(10, Color.BLACK));
-//
-//    }
 
 	@FXML
     public void setblur() {           
              imageView.setEffect(new GaussianBlur(5));
-//             mypane.setEffect(new GaussianBlur(10));
+
     }
 	
 	@FXML
