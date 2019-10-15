2c59f245b758649820c468ecf9940eb1e592a733
Ernesto Ramiro <atomms@gmail.com>
2019-10-15 17:18:28 +0200

    controller class created for initializing

--- BasicJavaFX/src/application/Main.java
+++ BasicJavaFX/src/application/Main.java
@@ -3,11 +3,8 @@ package application;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.effect.GaussianBlur;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -23,7 +20,7 @@ public class Main extends Application {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource(
 				"../view/forest.fxml"));
 
-		// y opcionalmente un controlador para inicializar algún componente
+		// añadimos un controlador para inicializar algún componente
 		loader.setController(new SetBlur());
 
 		// ahora sobre el panel mypane cargamos la vista que dejamos en el
@@ -35,22 +32,7 @@ public class Main extends Application {
 		stage.setScene(new Scene(mypane));
 		stage.show();
 	}
-
-	// la clase controladora que añade un efecto a la imagen
-	class SetBlur {
-
-		// la etiqueta FXML permite acceder al componente imageView ya creado en
-		// fxml
-		@FXML
-		private ImageView forestview;
-
-		@FXML
-		public void initialize() {
-			forestview.setEffect(new GaussianBlur(20));
-		}
-
-	}
-
+	
 	// arranca la aplicación
 	public static void main(String[] args) {
 		launch(args);
